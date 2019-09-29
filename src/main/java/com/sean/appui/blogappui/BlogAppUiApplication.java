f74a1f40deb748afa7498250512c6a6fe6f7985a
Sean Maxwell <katanablaydz@yahoo.com>
2019-09-29 14:48:14 -0500

    Initial commit

---
+++ src/main/java/com/sean/appui/blogappui/BlogAppUiApplication.java
@@ -0,0 +1,13 @@
+package com.sean.appui.blogappui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAppUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAppUiApplication.class, args);
+	}
+
+}
